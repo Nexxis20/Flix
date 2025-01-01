9c878c387427d759a7abd98a65b1b1724ba32381
Nexxis20 <nnexxus15@proton.me>
2025-01-01 17:16:53 -0300

    Update FilmesView.Designer.cs

--- Aniflix/FilmesView.Designer.cs
+++ Aniflix/FilmesView.Designer.cs
@@ -32,7 +32,6 @@
             autoLabel1 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel2 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel3 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
-            autoLabel4 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel5 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel6 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel7 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
@@ -40,15 +39,37 @@
             autoLabel9 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel10 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             autoLabel11 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
-            autoLabel17 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
-            textBoxExt1 = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesTituloText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesAudioBox = new ComboBox();
+            autoLabel12 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            FilmesSinopseText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesTituloOriginalText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            autoLabel4 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
+            FilmesDataLancamentoText = new MaskedTextBox();
+            FilmesFranquia = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesGeneroText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesTagsText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesEstrelasText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesDiretorText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesEstudioText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            FilmesResumoText = new Syncfusion.Windows.Forms.Tools.TextBoxExt();
+            autoLabel13 = new Syncfusion.Windows.Forms.Tools.AutoLabel();
             ((System.ComponentModel.ISupportInitialize)FilmesCodigoText).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)textBoxExt1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesTituloText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesSinopseText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesTituloOriginalText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesFranquia).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesGeneroText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesTagsText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesEstrelasText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesDiretorText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesEstudioText).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesResumoText).BeginInit();
             SuspendLayout();
             // 
             // FilmesCodigoText
             // 
-            FilmesCodigoText.BeforeTouchSize = new Size(123, 23);
+            FilmesCodigoText.BeforeTouchSize = new Size(612, 414);
             FilmesCodigoText.Location = new Point(12, 27);
             FilmesCodigoText.Name = "FilmesCodigoText";
             FilmesCodigoText.PlaceholderText = "Código do Filme";
@@ -74,23 +95,15 @@
             // 
             // autoLabel3
             // 
-            autoLabel3.Location = new Point(242, 297);
+            autoLabel3.Location = new Point(12, 53);
             autoLabel3.Name = "autoLabel3";
             autoLabel3.Size = new Size(48, 15);
             autoLabel3.TabIndex = 3;
             autoLabel3.Text = "Sinopse";
             // 
-            // autoLabel4
-            // 
-            autoLabel4.Location = new Point(427, 189);
-            autoLabel4.Name = "autoLabel4";
-            autoLabel4.Size = new Size(83, 15);
-            autoLabel4.TabIndex = 4;
-            autoLabel4.Text = "Título Original";
-            // 
             // autoLabel5
             // 
-            autoLabel5.Location = new Point(491, 297);
+            autoLabel5.Location = new Point(489, 180);
             autoLabel5.Name = "autoLabel5";
             autoLabel5.Size = new Size(116, 15);
             autoLabel5.TabIndex = 5;
@@ -98,7 +111,7 @@
             // 
             // autoLabel6
             // 
-            autoLabel6.Location = new Point(312, 601);
+            autoLabel6.Location = new Point(12, 224);
             autoLabel6.Name = "autoLabel6";
             autoLabel6.Size = new Size(53, 15);
             autoLabel6.TabIndex = 6;
@@ -106,15 +119,15 @@
             // 
             // autoLabel7
             // 
-            autoLabel7.Location = new Point(363, 443);
+            autoLabel7.Location = new Point(12, 268);
             autoLabel7.Name = "autoLabel7";
-            autoLabel7.Size = new Size(95, 15);
+            autoLabel7.Size = new Size(94, 15);
             autoLabel7.TabIndex = 7;
-            autoLabel7.Text = "Código do Filme";
+            autoLabel7.Text = "Gênero do Filme";
             // 
             // autoLabel8
             // 
-            autoLabel8.Location = new Point(752, 206);
+            autoLabel8.Location = new Point(471, 268);
             autoLabel8.Name = "autoLabel8";
             autoLabel8.Size = new Size(31, 15);
             autoLabel8.TabIndex = 8;
@@ -122,7 +135,7 @@
             // 
             // autoLabel9
             // 
-            autoLabel9.Location = new Point(512, 468);
+            autoLabel9.Location = new Point(12, 312);
             autoLabel9.Name = "autoLabel9";
             autoLabel9.Size = new Size(43, 15);
             autoLabel9.TabIndex = 9;
@@ -130,7 +143,7 @@
             // 
             // autoLabel10
             // 
-            autoLabel10.Location = new Point(688, 457);
+            autoLabel10.Location = new Point(12, 356);
             autoLabel10.Name = "autoLabel10";
             autoLabel10.Size = new Size(46, 15);
             autoLabel10.TabIndex = 10;
@@ -138,36 +151,167 @@
             // 
             // autoLabel11
             // 
-            autoLabel11.Location = new Point(839, 468);
+            autoLabel11.Location = new Point(12, 400);
             autoLabel11.Name = "autoLabel11";
             autoLabel11.Size = new Size(46, 15);
             autoLabel11.TabIndex = 11;
             autoLabel11.Text = "Estúdio";
             // 
-            // autoLabel17
+            // FilmesTituloText
             // 
-            autoLabel17.Location = new Point(748, 341);
-            autoLabel17.Name = "autoLabel17";
-            autoLabel17.Size = new Size(94, 15);
-            autoLabel17.TabIndex = 17;
-            autoLabel17.Text = "Gênero do Filme";
+            FilmesTituloText.BeforeTouchSize = new Size(612, 414);
+            FilmesTituloText.Location = new Point(141, 27);
+            FilmesTituloText.Name = "FilmesTituloText";
+            FilmesTituloText.PlaceholderText = "Título ";
+            FilmesTituloText.Size = new Size(324, 23);
+            FilmesTituloText.TabIndex = 18;
             // 
-            // textBoxExt1
+            // FilmesAudioBox
             // 
-            textBoxExt1.BeforeTouchSize = new Size(123, 23);
-            textBoxExt1.Location = new Point(141, 27);
-            textBoxExt1.Name = "textBoxExt1";
-            textBoxExt1.PlaceholderText = "Código do Filme";
-            textBoxExt1.Size = new Size(123, 23);
-            textBoxExt1.TabIndex = 18;
+            FilmesAudioBox.FormattingEnabled = true;
+            FilmesAudioBox.Items.AddRange(new object[] { "Dublado", "Legendado", "Nacional", "Desconhecido" });
+            FilmesAudioBox.Location = new Point(471, 27);
+            FilmesAudioBox.Name = "FilmesAudioBox";
+            FilmesAudioBox.Size = new Size(153, 23);
+            FilmesAudioBox.TabIndex = 19;
+            FilmesAudioBox.Text = "Tipo de Áudio";
+            // 
+            // autoLabel12
+            // 
+            autoLabel12.Location = new Point(471, 9);
+            autoLabel12.Name = "autoLabel12";
+            autoLabel12.Size = new Size(39, 15);
+            autoLabel12.TabIndex = 20;
+            autoLabel12.Text = "Áudio";
+            // 
+            // FilmesSinopseText
+            // 
+            FilmesSinopseText.BeforeTouchSize = new Size(612, 414);
+            FilmesSinopseText.Location = new Point(12, 71);
+            FilmesSinopseText.Multiline = true;
+            FilmesSinopseText.Name = "FilmesSinopseText";
+            FilmesSinopseText.PlaceholderText = "Sinopse";
+            FilmesSinopseText.Size = new Size(612, 106);
+            FilmesSinopseText.TabIndex = 22;
+            // 
+            // FilmesTituloOriginalText
+            // 
+            FilmesTituloOriginalText.BeforeTouchSize = new Size(612, 414);
+            FilmesTituloOriginalText.Location = new Point(12, 198);
+            FilmesTituloOriginalText.Name = "FilmesTituloOriginalText";
+            FilmesTituloOriginalText.PlaceholderText = "Título Original";
+            FilmesTituloOriginalText.Size = new Size(471, 23);
+            FilmesTituloOriginalText.TabIndex = 24;
+            // 
+            // autoLabel4
+            // 
+            autoLabel4.Location = new Point(12, 180);
+            autoLabel4.Name = "autoLabel4";
+            autoLabel4.Size = new Size(83, 15);
+            autoLabel4.TabIndex = 23;
+            autoLabel4.Text = "Título Original";
+            // 
+            // FilmesDataLancamentoText
+            // 
+            FilmesDataLancamentoText.Location = new Point(489, 198);
+            FilmesDataLancamentoText.Mask = "00/00/0000";
+            FilmesDataLancamentoText.Name = "FilmesDataLancamentoText";
+            FilmesDataLancamentoText.Size = new Size(135, 23);
+            FilmesDataLancamentoText.TabIndex = 25;
+            FilmesDataLancamentoText.ValidatingType = typeof(DateTime);
+            // 
+            // FilmesFranquia
+            // 
+            FilmesFranquia.BeforeTouchSize = new Size(612, 414);
+            FilmesFranquia.Location = new Point(12, 242);
+            FilmesFranquia.Name = "FilmesFranquia";
+            FilmesFranquia.PlaceholderText = "Franquia";
+            FilmesFranquia.Size = new Size(612, 23);
+            FilmesFranquia.TabIndex = 26;
+            // 
+            // FilmesGeneroText
+            // 
+            FilmesGeneroText.BeforeTouchSize = new Size(612, 414);
+            FilmesGeneroText.Location = new Point(12, 286);
+            FilmesGeneroText.Name = "FilmesGeneroText";
+            FilmesGeneroText.PlaceholderText = "Gênero do Filme";
+            FilmesGeneroText.Size = new Size(453, 23);
+            FilmesGeneroText.TabIndex = 27;
+            // 
+            // FilmesTagsText
+            // 
+            FilmesTagsText.BeforeTouchSize = new Size(612, 414);
+            FilmesTagsText.Location = new Point(471, 286);
+            FilmesTagsText.Name = "FilmesTagsText";
+            FilmesTagsText.PlaceholderText = "Tags";
+            FilmesTagsText.Size = new Size(153, 23);
+            FilmesTagsText.TabIndex = 28;
+            // 
+            // FilmesEstrelasText
+            // 
+            FilmesEstrelasText.BeforeTouchSize = new Size(612, 414);
+            FilmesEstrelasText.Location = new Point(12, 374);
+            FilmesEstrelasText.Name = "FilmesEstrelasText";
+            FilmesEstrelasText.PlaceholderText = "Estrelas";
+            FilmesEstrelasText.Size = new Size(612, 23);
+            FilmesEstrelasText.TabIndex = 29;
+            // 
+            // FilmesDiretorText
+            // 
+            FilmesDiretorText.BeforeTouchSize = new Size(612, 414);
+            FilmesDiretorText.Location = new Point(12, 330);
+            FilmesDiretorText.Name = "FilmesDiretorText";
+            FilmesDiretorText.PlaceholderText = "Diretor";
+            FilmesDiretorText.Size = new Size(612, 23);
+            FilmesDiretorText.TabIndex = 30;
+            // 
+            // FilmesEstudioText
+            // 
+            FilmesEstudioText.BeforeTouchSize = new Size(612, 414);
+            FilmesEstudioText.Location = new Point(12, 418);
+            FilmesEstudioText.Name = "FilmesEstudioText";
+            FilmesEstudioText.PlaceholderText = "Estúdio";
+            FilmesEstudioText.Size = new Size(612, 23);
+            FilmesEstudioText.TabIndex = 31;
+            // 
+            // FilmesResumoText
+            // 
+            FilmesResumoText.BeforeTouchSize = new Size(612, 414);
+            FilmesResumoText.Location = new Point(630, 27);
+            FilmesResumoText.Multiline = true;
+            FilmesResumoText.Name = "FilmesResumoText";
+            FilmesResumoText.PlaceholderText = "Resumo";
+            FilmesResumoText.Size = new Size(612, 414);
+            FilmesResumoText.TabIndex = 32;
+            // 
+            // autoLabel13
+            // 
+            autoLabel13.Location = new Point(630, 9);
+            autoLabel13.Name = "autoLabel13";
+            autoLabel13.Size = new Size(50, 15);
+            autoLabel13.TabIndex = 33;
+            autoLabel13.Text = "Resumo";
             // 
             // FilmesView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1692, 975);
-            Controls.Add(textBoxExt1);
-            Controls.Add(autoLabel17);
+            ClientSize = new Size(1257, 562);
+            Controls.Add(autoLabel13);
+            Controls.Add(FilmesResumoText);
+            Controls.Add(FilmesEstudioText);
+            Controls.Add(FilmesDiretorText);
+            Controls.Add(FilmesEstrelasText);
+            Controls.Add(FilmesTagsText);
+            Controls.Add(FilmesGeneroText);
+            Controls.Add(FilmesFranquia);
+            Controls.Add(FilmesDataLancamentoText);
+            Controls.Add(FilmesTituloOriginalText);
+            Controls.Add(autoLabel4);
+            Controls.Add(FilmesSinopseText);
+            Controls.Add(autoLabel12);
+            Controls.Add(FilmesAudioBox);
+            Controls.Add(FilmesTituloText);
             Controls.Add(autoLabel11);
             Controls.Add(autoLabel10);
             Controls.Add(autoLabel9);
@@ -175,15 +319,25 @@
             Controls.Add(autoLabel7);
             Controls.Add(autoLabel6);
             Controls.Add(autoLabel5);
-            Controls.Add(autoLabel4);
             Controls.Add(autoLabel3);
             Controls.Add(autoLabel2);
             Controls.Add(autoLabel1);
             Controls.Add(FilmesCodigoText);
             Name = "FilmesView";
+            Style.MdiChild.IconHorizontalAlignment = HorizontalAlignment.Center;
+            Style.MdiChild.IconVerticalAlignment = System.Windows.Forms.VisualStyles.VerticalAlignment.Center;
             Text = "FilmesView";
             ((System.ComponentModel.ISupportInitialize)FilmesCodigoText).EndInit();
-            ((System.ComponentModel.ISupportInitialize)textBoxExt1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesTituloText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesSinopseText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesTituloOriginalText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesFranquia).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesGeneroText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesTagsText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesEstrelasText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesDiretorText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesEstudioText).EndInit();
+            ((System.ComponentModel.ISupportInitialize)FilmesResumoText).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -194,7 +348,6 @@
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel1;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel2;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel3;
-        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel4;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel5;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel6;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel7;
@@ -202,7 +355,20 @@
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel9;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel10;
         private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel11;
-        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel17;
-        private Syncfusion.Windows.Forms.Tools.TextBoxExt textBoxExt1;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesTituloText;
+        private ComboBox FilmesAudioBox;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel12;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesSinopseText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesTituloOriginalText;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel4;
+        private MaskedTextBox FilmesDataLancamentoText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesFranquia;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesGeneroText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesTagsText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesEstrelasText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesDiretorText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesEstudioText;
+        private Syncfusion.Windows.Forms.Tools.TextBoxExt FilmesResumoText;
+        private Syncfusion.Windows.Forms.Tools.AutoLabel autoLabel13;
     }
 }
