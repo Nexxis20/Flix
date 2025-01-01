7475d9b0d5f281dc3d87ccb1dcddd9c66ef2ec3d
Nexxis20 <nnexxus15@proton.me>
2025-01-01 12:44:30 -0300

    Update Program.cs

--- Aniflix/Program.cs
+++ Aniflix/Program.cs
@@ -2,14 +2,10 @@ namespace Aniflix
 {
     internal static class Program
     {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            //Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzY1MTA5NUAzMjM4MmUzMDJlMzBnZFVTUVlZUHR0YzFlU2gwU3Y0UUtqenpleVFjZlMyQy9Mc2Vpb21uSzAwPQ==");
             ApplicationConfiguration.Initialize();
             Application.Run(new AniflixView());
         }
