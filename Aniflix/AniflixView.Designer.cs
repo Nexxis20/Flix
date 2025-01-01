02f3b4798ed58938ac0a800764a92449f0d9d86e
Nexxis20 <nnexxus15@proton.me>
2025-01-01 12:54:54 -0300

    Update AniflixView.Designer.cs

--- Aniflix/AniflixView.Designer.cs
+++ Aniflix/AniflixView.Designer.cs
@@ -28,139 +28,181 @@
         /// </summary>
         private void InitializeComponent()
         {
-            sfButton1 = new Syncfusion.WinForms.Controls.SfButton();
+            Filmes = new Syncfusion.WinForms.Controls.SfButton();
+            Sair = new Syncfusion.WinForms.Controls.SfButton();
+            Series = new Syncfusion.WinForms.Controls.SfButton();
+            GoreButton = new Syncfusion.WinForms.Controls.SfButton();
+            BreakOut = new Syncfusion.WinForms.Controls.SfButton();
+            Animes = new Syncfusion.WinForms.Controls.SfButton();
+            GoreFilmes = new Syncfusion.WinForms.Controls.SfButton();
             sfButton2 = new Syncfusion.WinForms.Controls.SfButton();
-            sfButton3 = new Syncfusion.WinForms.Controls.SfButton();
-            sfButton4 = new Syncfusion.WinForms.Controls.SfButton();
-            sfButton5 = new Syncfusion.WinForms.Controls.SfButton();
-            sfButton6 = new Syncfusion.WinForms.Controls.SfButton();
             SuspendLayout();
             // 
-            // sfButton1
+            // Filmes
             // 
-            sfButton1.BackColor = Color.FromArgb(52, 152, 219);
-            sfButton1.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
-            sfButton1.ForeColor = Color.White;
-            sfButton1.ImageSize = new Size(48, 48);
-            sfButton1.Location = new Point(221, 163);
-            sfButton1.Name = "sfButton1";
-            sfButton1.Size = new Size(180, 206);
-            sfButton1.Style.BackColor = Color.FromArgb(52, 152, 219);
-            sfButton1.Style.ForeColor = Color.White;
-            sfButton1.Style.Image = Properties.Resources.filmstrip;
-            sfButton1.Style.ImageForeColor = Color.White;
-            sfButton1.TabIndex = 0;
-            sfButton1.Text = "Filmes";
-            sfButton1.TextAlign = ContentAlignment.BottomLeft;
-            sfButton1.TextImageRelation = TextImageRelation.Overlay;
-            sfButton1.UseVisualStyleBackColor = false;
+            Filmes.BackColor = Color.FromArgb(52, 152, 219);
+            Filmes.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            Filmes.ForeColor = Color.White;
+            Filmes.ImageSize = new Size(48, 48);
+            Filmes.Location = new Point(277, 163);
+            Filmes.Name = "Filmes";
+            Filmes.Size = new Size(124, 108);
+            Filmes.Style.BackColor = Color.FromArgb(52, 152, 219);
+            Filmes.Style.ForeColor = Color.White;
+            Filmes.Style.Image = Properties.Resources.filmstrip;
+            Filmes.Style.ImageForeColor = Color.White;
+            Filmes.TabIndex = 0;
+            Filmes.Text = "Filmes";
+            Filmes.TextAlign = ContentAlignment.BottomLeft;
+            Filmes.TextImageRelation = TextImageRelation.Overlay;
+            Filmes.UseVisualStyleBackColor = false;
+            // 
+            // Sair
+            // 
+            Sair.BackColor = Color.FromArgb(211, 72, 38);
+            Sair.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            Sair.ForeColor = Color.White;
+            Sair.ImageSize = new Size(48, 48);
+            Sair.Location = new Point(572, 375);
+            Sair.Name = "Sair";
+            Sair.Size = new Size(159, 92);
+            Sair.Style.BackColor = Color.FromArgb(211, 72, 38);
+            Sair.Style.ForeColor = Color.White;
+            Sair.Style.Image = Properties.Resources.exit_to_app;
+            Sair.Style.ImageForeColor = Color.White;
+            Sair.TabIndex = 1;
+            Sair.Text = "Sair";
+            Sair.TextAlign = ContentAlignment.BottomLeft;
+            Sair.TextImageRelation = TextImageRelation.Overlay;
+            Sair.UseVisualStyleBackColor = false;
+            // 
+            // Series
+            // 
+            Series.BackColor = Color.FromArgb(34, 132, 121);
+            Series.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            Series.ForeColor = Color.White;
+            Series.ImageSize = new Size(48, 48);
+            Series.Location = new Point(277, 277);
+            Series.Name = "Series";
+            Series.Size = new Size(124, 92);
+            Series.Style.BackColor = Color.FromArgb(34, 132, 121);
+            Series.Style.ForeColor = Color.White;
+            Series.Style.Image = Properties.Resources.camcorder;
+            Series.Style.ImageForeColor = Color.White;
+            Series.TabIndex = 2;
+            Series.Text = "Séries";
+            Series.TextAlign = ContentAlignment.BottomLeft;
+            Series.TextImageRelation = TextImageRelation.Overlay;
+            Series.UseVisualStyleBackColor = false;
+            // 
+            // GoreButton
+            // 
+            GoreButton.BackColor = Color.FromArgb(220, 97, 65);
+            GoreButton.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            GoreButton.ForeColor = Color.White;
+            GoreButton.ImageSize = new Size(48, 48);
+            GoreButton.Location = new Point(277, 375);
+            GoreButton.Name = "GoreButton";
+            GoreButton.Size = new Size(124, 92);
+            GoreButton.Style.BackColor = Color.FromArgb(220, 97, 65);
+            GoreButton.Style.ForeColor = Color.White;
+            GoreButton.Style.Image = Properties.Resources.balloon;
+            GoreButton.Style.ImageForeColor = Color.White;
+            GoreButton.TabIndex = 3;
+            GoreButton.Text = "Gore";
+            GoreButton.TextAlign = ContentAlignment.BottomLeft;
+            GoreButton.TextImageRelation = TextImageRelation.Overlay;
+            GoreButton.UseVisualStyleBackColor = false;
+            // 
+            // BreakOut
+            // 
+            BreakOut.BackColor = Color.FromArgb(82, 0, 101);
+            BreakOut.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            BreakOut.ForeColor = Color.White;
+            BreakOut.ImageSize = new Size(48, 48);
+            BreakOut.Location = new Point(407, 277);
+            BreakOut.Name = "BreakOut";
+            BreakOut.Size = new Size(159, 92);
+            BreakOut.Style.BackColor = Color.FromArgb(82, 0, 101);
+            BreakOut.Style.ForeColor = Color.White;
+            BreakOut.Style.Image = Properties.Resources.babel;
+            BreakOut.Style.ImageForeColor = Color.White;
+            BreakOut.TabIndex = 4;
+            BreakOut.Text = "BreakOut - Filmes";
+            BreakOut.TextAlign = ContentAlignment.BottomLeft;
+            BreakOut.TextImageRelation = TextImageRelation.Overlay;
+            BreakOut.UseVisualStyleBackColor = false;
+            // 
+            // Animes
+            // 
+            Animes.BackColor = Color.FromArgb(96, 61, 188);
+            Animes.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            Animes.ForeColor = Color.White;
+            Animes.ImageSize = new Size(48, 48);
+            Animes.Location = new Point(407, 163);
+            Animes.Name = "Animes";
+            Animes.Size = new Size(324, 108);
+            Animes.Style.BackColor = Color.FromArgb(96, 61, 188);
+            Animes.Style.ForeColor = Color.White;
+            Animes.Style.Image = Properties.Resources.animation_play;
+            Animes.Style.ImageForeColor = Color.White;
+            Animes.TabIndex = 5;
+            Animes.Text = "Animes";
+            Animes.TextAlign = ContentAlignment.BottomLeft;
+            Animes.TextImageRelation = TextImageRelation.Overlay;
+            Animes.UseVisualStyleBackColor = false;
+            // 
+            // GoreFilmes
+            // 
+            GoreFilmes.BackColor = Color.FromArgb(220, 97, 65);
+            GoreFilmes.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
+            GoreFilmes.ForeColor = Color.White;
+            GoreFilmes.ImageSize = new Size(48, 48);
+            GoreFilmes.Location = new Point(407, 375);
+            GoreFilmes.Name = "GoreFilmes";
+            GoreFilmes.Size = new Size(159, 92);
+            GoreFilmes.Style.BackColor = Color.FromArgb(220, 97, 65);
+            GoreFilmes.Style.ForeColor = Color.White;
+            GoreFilmes.Style.Image = Properties.Resources.balloon;
+            GoreFilmes.Style.ImageForeColor = Color.White;
+            GoreFilmes.TabIndex = 6;
+            GoreFilmes.Text = "Gore - Séries";
+            GoreFilmes.TextAlign = ContentAlignment.BottomLeft;
+            GoreFilmes.TextImageRelation = TextImageRelation.Overlay;
+            GoreFilmes.UseVisualStyleBackColor = false;
             // 
             // sfButton2
             // 
-            sfButton2.BackColor = Color.FromArgb(211, 72, 38);
+            sfButton2.BackColor = Color.FromArgb(82, 0, 101);
             sfButton2.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
             sfButton2.ForeColor = Color.White;
             sfButton2.ImageSize = new Size(48, 48);
-            sfButton2.Location = new Point(407, 163);
+            sfButton2.Location = new Point(572, 277);
             sfButton2.Name = "sfButton2";
-            sfButton2.Size = new Size(300, 108);
-            sfButton2.Style.BackColor = Color.FromArgb(211, 72, 38);
+            sfButton2.Size = new Size(159, 92);
+            sfButton2.Style.BackColor = Color.FromArgb(82, 0, 101);
             sfButton2.Style.ForeColor = Color.White;
-            sfButton2.Style.Image = Properties.Resources.exit_to_app;
+            sfButton2.Style.Image = Properties.Resources.babel;
             sfButton2.Style.ImageForeColor = Color.White;
-            sfButton2.TabIndex = 1;
-            sfButton2.Text = "Sair";
+            sfButton2.TabIndex = 7;
+            sfButton2.Text = "BreakOut - Séries";
             sfButton2.TextAlign = ContentAlignment.BottomLeft;
             sfButton2.TextImageRelation = TextImageRelation.Overlay;
             sfButton2.UseVisualStyleBackColor = false;
-            // 
-            // sfButton3
-            // 
-            sfButton3.BackColor = Color.FromArgb(34, 132, 121);
-            sfButton3.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
-            sfButton3.ForeColor = Color.White;
-            sfButton3.ImageSize = new Size(48, 48);
-            sfButton3.Location = new Point(407, 277);
-            sfButton3.Name = "sfButton3";
-            sfButton3.Size = new Size(96, 92);
-            sfButton3.Style.BackColor = Color.FromArgb(34, 132, 121);
-            sfButton3.Style.ForeColor = Color.White;
-            sfButton3.Style.Image = Properties.Resources.camcorder;
-            sfButton3.Style.ImageForeColor = Color.White;
-            sfButton3.TabIndex = 2;
-            sfButton3.Text = "Séries";
-            sfButton3.TextAlign = ContentAlignment.BottomLeft;
-            sfButton3.TextImageRelation = TextImageRelation.Overlay;
-            sfButton3.UseVisualStyleBackColor = false;
-            // 
-            // sfButton4
-            // 
-            sfButton4.BackColor = Color.FromArgb(220, 97, 65);
-            sfButton4.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
-            sfButton4.ForeColor = Color.White;
-            sfButton4.ImageSize = new Size(48, 48);
-            sfButton4.Location = new Point(509, 277);
-            sfButton4.Name = "sfButton4";
-            sfButton4.Size = new Size(96, 92);
-            sfButton4.Style.BackColor = Color.FromArgb(220, 97, 65);
-            sfButton4.Style.ForeColor = Color.White;
-            sfButton4.Style.Image = Properties.Resources.balloon;
-            sfButton4.Style.ImageForeColor = Color.White;
-            sfButton4.TabIndex = 3;
-            sfButton4.Text = "Gore";
-            sfButton4.TextAlign = ContentAlignment.BottomLeft;
-            sfButton4.TextImageRelation = TextImageRelation.Overlay;
-            sfButton4.UseVisualStyleBackColor = false;
-            // 
-            // sfButton5
-            // 
-            sfButton5.BackColor = Color.FromArgb(82, 0, 101);
-            sfButton5.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
-            sfButton5.ForeColor = Color.White;
-            sfButton5.ImageSize = new Size(48, 48);
-            sfButton5.Location = new Point(611, 277);
-            sfButton5.Name = "sfButton5";
-            sfButton5.Size = new Size(96, 92);
-            sfButton5.Style.BackColor = Color.FromArgb(82, 0, 101);
-            sfButton5.Style.ForeColor = Color.White;
-            sfButton5.Style.Image = Properties.Resources.babel;
-            sfButton5.Style.ImageForeColor = Color.White;
-            sfButton5.TabIndex = 4;
-            sfButton5.Text = "BreakOut";
-            sfButton5.TextAlign = ContentAlignment.BottomLeft;
-            sfButton5.TextImageRelation = TextImageRelation.Overlay;
-            sfButton5.UseVisualStyleBackColor = false;
-            // 
-            // sfButton6
-            // 
-            sfButton6.BackColor = Color.FromArgb(96, 61, 188);
-            sfButton6.Font = new Font("Inconsolata", 11.25F, FontStyle.Bold);
-            sfButton6.ForeColor = Color.White;
-            sfButton6.ImageSize = new Size(48, 48);
-            sfButton6.Location = new Point(221, 375);
-            sfButton6.Name = "sfButton6";
-            sfButton6.Size = new Size(486, 143);
-            sfButton6.Style.BackColor = Color.FromArgb(96, 61, 188);
-            sfButton6.Style.ForeColor = Color.White;
-            sfButton6.Style.Image = Properties.Resources.animation_play;
-            sfButton6.Style.ImageForeColor = Color.White;
-            sfButton6.TabIndex = 5;
-            sfButton6.Text = "Animes";
-            sfButton6.TextAlign = ContentAlignment.BottomLeft;
-            sfButton6.TextImageRelation = TextImageRelation.Overlay;
-            sfButton6.UseVisualStyleBackColor = false;
             // 
             // AniflixView
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(928, 681);
-            Controls.Add(sfButton6);
-            Controls.Add(sfButton5);
-            Controls.Add(sfButton4);
-            Controls.Add(sfButton3);
+            ClientSize = new Size(1054, 624);
             Controls.Add(sfButton2);
-            Controls.Add(sfButton1);
+            Controls.Add(GoreFilmes);
+            Controls.Add(Animes);
+            Controls.Add(BreakOut);
+            Controls.Add(GoreButton);
+            Controls.Add(Series);
+            Controls.Add(Sair);
+            Controls.Add(Filmes);
             MaximizeBox = false;
             Name = "AniflixView";
             StartPosition = FormStartPosition.CenterScreen;
@@ -172,11 +214,13 @@
 
         #endregion
 
-        private Syncfusion.WinForms.Controls.SfButton sfButton1;
+        private Syncfusion.WinForms.Controls.SfButton Filmes;
+        private Syncfusion.WinForms.Controls.SfButton Sair;
+        private Syncfusion.WinForms.Controls.SfButton Series;
+        private Syncfusion.WinForms.Controls.SfButton GoreButton;
+        private Syncfusion.WinForms.Controls.SfButton BreakOut;
+        private Syncfusion.WinForms.Controls.SfButton Animes;
+        private Syncfusion.WinForms.Controls.SfButton GoreFilmes;
         private Syncfusion.WinForms.Controls.SfButton sfButton2;
-        private Syncfusion.WinForms.Controls.SfButton sfButton3;
-        private Syncfusion.WinForms.Controls.SfButton sfButton4;
-        private Syncfusion.WinForms.Controls.SfButton sfButton5;
-        private Syncfusion.WinForms.Controls.SfButton sfButton6;
     }
 }
