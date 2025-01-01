055724f199779d1c052b47ff9a7f6e2d25331dce
Nexxis20 <nnexxus15@proton.me>
2025-01-01 18:47:26 -0300

    Update FilmesModel.cs

--- Aniflix/Model/FilmesModel.cs
+++ Aniflix/Model/FilmesModel.cs
@@ -1,6 +1,25 @@
-﻿namespace Aniflix.Models
+﻿namespace Aniflix.Model
 {
-    internal class FilmesModel
+    public class FilmesModel
     {
+        private string Codigo;
+        private string Titulo;
+        private string Sinopse;
+        private string TituloOriginal;
+        private string DataLancamento;
+        private string Franquia;
+        private string Genero;
+        private string Tags;
+        private string Diretor;
+        private string Estrelas;
+        private string Estudio;
+        private string Audio;
+
+        // getters and setters
+
+
+
+
+
     }
 }
