71c4a09254c44dd4dc25271bb056979a650ec14c
Nexxis20 <nnexxus15@proton.me>
2025-01-01 16:15:30 -0300

    Update all files

--- Aniflix/FilmesView.cs
+++ Aniflix/FilmesView.cs
@@ -1,11 +1,27 @@
-﻿namespace Aniflix
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Aniflix
 {
     public partial class FilmesView : Form
     {
+        DateTime dataLancamento;
+        private IConfiguration? _configuration;
         public FilmesView()
         {
             InitializeComponent();
+            ConfigureAppSettings();
         }
+        private void ConfigureAppSettings()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            _configuration = builder.Build();
+        }
+
+
+
         private void FilmesCodigoText_KeyPress(object sender, KeyPressEventArgs e)
         {
             if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
