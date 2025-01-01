6e42f770aa69aafb61971129836ae2721e4a0377
Nexxis20 <nnexxus15@proton.me>
2025-01-01 16:08:34 -0300

    Update FilmesView.cs

--- Aniflix/FilmesView.cs
+++ Aniflix/FilmesView.cs
@@ -9,14 +9,9 @@
         private void FilmesCodigoText_KeyPress(object sender, KeyPressEventArgs e)
         {
 
-            if (!(char.IsControl(e.KeyChar) || char.IsDigit(e.KeyChar) || e.KeyChar == '.'))
+            if (!char.IsControl(e.KeyChar) || !char.IsDigit(e.KeyChar))
             {
-                e.Handled = false;
-            }
-
-            if ((e.KeyChar == '.') && (((TextBox)sender).Text.IndexOf('.') > -1))
-            {
-                e.Handled = false;
+                e.Handled = true;
             }
         }
     }
