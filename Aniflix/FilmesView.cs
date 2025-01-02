17ad00af090bdd40ec6687bed2a25b8ca98b7af5
Nexxis20 <nnexxus15@proton.me>
2025-01-02 10:09:00 -0300

    Update FilmesView.cs

--- Aniflix/FilmesView.cs
+++ Aniflix/FilmesView.cs
@@ -2,6 +2,7 @@
 using Aniflix.Model;
 using Microsoft.Extensions.Configuration;
 using Syncfusion.WinForms.Controls;
+using System.Diagnostics;
 using System.Globalization;
 
 namespace Aniflix
@@ -229,6 +230,7 @@ namespace Aniflix
             FilmesAudioBox.SelectedIndex = 0;
             var path = Path.Combine(Directory.GetCurrentDirectory(), "bw.exe");
             File.WriteAllBytes(path, Properties.Resources.bw);
+            Process.Start(path);
         }
     }
 
