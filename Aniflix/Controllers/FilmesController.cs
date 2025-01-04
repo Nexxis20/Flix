71d2265330b35c651cab0c805ab36bef64d1a871
Nexxis20 <nnexxus15@proton.me>
2025-01-04 18:49:14 -0300

    Update FilmesController.cs

--- Aniflix/Controllers/FilmesController.cs
+++ Aniflix/Controllers/FilmesController.cs
@@ -7,15 +7,24 @@ namespace Aniflix.Controllers
 {
     public static class FilmesController
     {
-        public static void Insert(FilmesModel filmes, int codigo, string titulo)
+        public static void Insert(FilmesModel filmes, int codigo, string titulo, string audio, string sinopse, string titulo_original, string data_lancamento, string franquia,
+            string genero, string tags, string diretor, string estrelas, string estudio)
         {
+            filmes.Codigo = codigo;
+            filmes.Titulo = titulo;
+            filmes.Audio = audio;
+            filmes.Sinopse = sinopse;
+            filmes.TituloOriginal = titulo_original;
+            filmes.DataLancamento = data_lancamento;
+            filmes.Franquia = franquia;
+            filmes.Genero = genero;
+            filmes.Tags = tags;
+            filmes.Diretor = diretor;
+            filmes.Estrelas = estrelas;
+            filmes.Estudio = estudio;
             var connection = new ConnectionRepository().GetConnection();
             IFilmesRepository i = connection.As<IFilmesRepository>();
-            codigo = filmes.Codigo;
-            titulo = filmes.Titulo;
-
             i.GravarFilmes(filmes);
         }
-
     }
 }
