58c5940cdc2c5028803e5a9db3ea306268f5f3a0
Nexxis20 <nnexxus15@proton.me>
2025-01-05 00:08:54 -0300

    Update IFilmesRepository.cs

--- Aniflix/Interfaces/IFilmesRepository.cs
+++ Aniflix/Interfaces/IFilmesRepository.cs
@@ -1,10 +1,10 @@
-﻿using Aniflix.Model;
-
-namespace Aniflix.Interfaces
+﻿namespace Aniflix.Interfaces
 {
     public interface IFilmesRepository
     {
-        void GravarFilmes(Filmes filmes);
-        List<Filmes> ListaFilmes();
+        void GravarFilmes(
+         string p_codigo, string p_titulo, string p_audio, string p_sinopse,
+         string p_titulo_original, string p_data_lancamento, string p_franquia,
+         string p_genero, string p_tags, string p_diretor, string p_estrelas, string p_estudio);
     }
 }
