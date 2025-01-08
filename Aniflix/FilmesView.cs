66fa3c651eb24ac1b20ed941fa26ba44f59f353e
Nexxis20 <nnexxus15@proton.me>
2025-01-08 20:47:26 -0300

    FilmesView.cs

--- Aniflix/FilmesView.cs
+++ Aniflix/FilmesView.cs
@@ -378,7 +378,7 @@ namespace Aniflix
                 filmes.Estrelas!, filmes.Estudio!, filmes.MCU!
             );
             MessageBox.Show("Filme " + filmes.Titulo + " inserido com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            Enabled = false;
+            this.Enabled = false;
         }
 
         private void Voltar_Click(object sender, EventArgs e)
