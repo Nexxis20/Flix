f928152f5ed8c04c7207ece80af5c5e4d02bab59
Nexxis20 <nnexxus15@proton.me>
2025-01-14 20:03:23 -0300

    FilmesView.cs

--- Aniflix/FilmesView.cs
+++ Aniflix/FilmesView.cs
@@ -300,7 +300,6 @@ namespace Aniflix
         private void FilmesView_Load(object sender, EventArgs e)
         {
             FilmesAudioBox.SelectedIndex = 0;
-            LoadFirstRecord();
             LoadFilmes();
             FilmesFaseMCUText.Text = "--";
             FilmesFranquiaText.Text = "--";
